96169e0bd136d2a19b4444193d8acbbc32bead20
Unknown <negron.adam.a@gmail.com>
2018-12-11 14:19:28 -0600

    WorldEngineStarted

---
+++ src/WorldEngine/WorldEngineMain.java
@@ -0,0 +1,10 @@
+package WorldEngine;
+
+public class WorldEngineMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the World Engine!");
+	}
+
+}
