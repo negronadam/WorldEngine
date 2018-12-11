6bb1a05a91b77a028e0e2e3a3b71adc7cf84fadd
Unknown <negron.adam.a@gmail.com>
2018-12-11 14:48:57 -0600

    MoreStuff

--- src/WorldEngine/WorldEngineMain.java
+++ src/WorldEngine/WorldEngineMain.java
@@ -1,10 +1,20 @@
 package WorldEngine;
-
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
 public class WorldEngineMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to the World Engine!");
+		System.out.println("The Game Engine Written by: Adam Negron");
+		boolean gameIsRunning = true;
+		while(gameIsRunning = true)
+		{
+			System.out.println("Welcome to the main menu!");
+			input.nextLine();
+		}
 	}
 
 }
