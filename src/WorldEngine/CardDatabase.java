dacfc0cad74afe3251f1f6157a7cb866e5793c5f
Unknown <negron.adam.a@gmail.com>
2018-12-19 11:25:11 -0600

    Creating the CardDatabase

--- src/WorldEngine/CardDatabase.java
+++ src/WorldEngine/CardDatabase.java
@@ -1,5 +1,18 @@
 package WorldEngine;
-
+import java.util.ArrayList;
 public class CardDatabase {
-
+	public CardDatabase()
+	{
+		ArrayList<CardClass> cardData = new ArrayList<CardClass>();
+		cardData.add(new CardClass("Lightning Resource", 0001, 10, 10, 1));
+		cardData.add(new CardClass("Water Resource", 0002, 10, 10, 2));
+		cardData.add(new CardClass("Fire Resource", 0003, 10, 10, 3));
+		cardData.add(new CardClass("Earth Resource", 0004, 10, 10, 4));
+		cardData.add(new CardClass("Panzer-Hydra", 0005, 1000, 2500, 5));
+	}
+	
+	public void cardSearch(int qCardID)
+	{
+		int searchCardID = qCardID;
+	}
 }
