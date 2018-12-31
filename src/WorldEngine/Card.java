37061059881f3e63dd5fcf2446aeaf3ea81d2f71
Dell-ADAM <negron.adam.a@gmail.com>
2018-12-31 16:19:30 -0600

    Started on the Battle System

--- src/WorldEngine/Card.java
+++ src/WorldEngine/Card.java
@@ -1,5 +1,28 @@
 package WorldEngine;
 
-public class Card {
+import java.util.Random;
 
+public class Card {
+	Random generator = new Random();
+	private int cardID;
+	private int cardNumbers;
+	private double cardHP;
+	private double cardMP;
+	private String cardName;
+	
+	public Card(String xCardName,int xCardID, double xCardHP, double xCardMP, int xCardNumbers)
+	{
+		xCardName = cardName;
+		xCardID = cardID;
+		xCardHP = cardHP;
+		xCardMP = cardMP;
+		xCardNumbers = cardNumbers;
+	}
+	
+	public void getCard (int cardNumber)
+	{
+		
+	}
+	
+	
 }
--- src/WorldEngine/CardDatabase.java
+++ src/WorldEngine/CardDatabase.java
@@ -3,17 +3,15 @@ import java.util.ArrayList;
 public class CardDatabase {
 	public CardDatabase()
 	{
-		ArrayList<CardClass> cardData = new ArrayList<CardClass>();
-		cardData.add(new CardClass("Lightning Resource", 0001, 10, 10, 1));
-		cardData.add(new CardClass("Water Resource", 0002, 10, 10, 2));
-		cardData.add(new CardClass("Fire Resource", 0003, 10, 10, 3));
-		cardData.add(new CardClass("Earth Resource", 0004, 10, 10, 4));
-		cardData.add(new CardClass("Panzer-Hydra", 0005, 1000, 2500, 5));
-		cardData.add(new CardClass("Knight", 0006, 500, 750, 6));
+		//Card Data will be stored here and accessible in the main method.
+		ArrayList<Card> cardData = new ArrayList<Card>();
+		cardData.add(new Card("Lightning Resource", 0001, 10, 10, 1));
+		cardData.add(new Card("Water Resource", 0002, 10, 10, 2));
+		cardData.add(new Card("Fire Resource", 0003, 10, 10, 3));
+		cardData.add(new Card("Earth Resource", 0004, 10, 10, 4));
+		cardData.add(new Card("Panzer-Hydra", 0005, 1000, 2500, 5));
+		cardData.add(new Card("Knight", 0006, 500, 750, 6));
+		cardData.add(new Card("Warrior" , 0007, 800, 750, 7));
 	}
 	
-	public void cardSearch(int qCardID)
-	{
-		int searchCardID = qCardID;
-	}
 }
