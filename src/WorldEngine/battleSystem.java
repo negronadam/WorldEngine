2a2a5bfb47904fd4ad9d0c2f872f5753480602ed
Dell-ADAM <negron.adam.a@gmail.com>
2018-12-31 16:38:40 -0600

    Making the CardDatabase accessible to other classes

--- src/WorldEngine/battleSystem.java
+++ src/WorldEngine/battleSystem.java
@@ -1,5 +1,19 @@
 package WorldEngine;
-
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 public class battleSystem {
-
+	public battleSystem()
+	{
+		CardDatabase database = new CardDatabase();
+	}
+	
+	public void getCard()
+	{
+		for(int a = 0; a < cardData.size; a++)
+		{
+			
+		}
+	}
+	
 }
