cc0033012395ea542b1862b0a44314c5230a8b43
Adam <negron.adam.a@gmail.com>
2019-01-17 13:25:18 -0600

    More Stuff

--- src/WorldEngine/ComponentTest.java
+++ src/WorldEngine/ComponentTest.java
@@ -1,5 +1,15 @@
 package WorldEngine;
-
-public class ComponentTest {
-
+import java.awt.*;
+public class ComponentTest extends Panel {
+	TextField text;
+	List list;
+	public ComponentTest()
+	{
+		text = new TextField();
+		list = new List();
+		list.setMultipleSelections(false);
+		setLayout(new BorderLayout());
+		add("North", text);
+		add("Center" , list);
+	}
 }
--- src/WorldEngine/UITest1.java
+++ src/WorldEngine/UITest1.java
@@ -1,22 +1,28 @@
 package WorldEngine;
+import java.util.Scanner;
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import java.net.*;
+import javax.swing.JPanel;
 public class UITest1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 		JFrame frame = new JFrame();
 		JDialog dialog = new JDialog();
 		JLabel label1 = new JLabel();
+		JPanel panel = new JPanel();
 		JOptionPane pane = new JOptionPane();
+		TextArea textarea = new TextArea();
 		frame.setSize(300, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("UI Test 1");
 		JOptionPane.showMessageDialog(null, "You've been Gnomed... Owo", "Gnomifier" , JOptionPane.ERROR_MESSAGE);
 		System.out.println("Enjoy being gnomed?");
+		textarea = new TextArea();
 		frame.setVisible(true);
 	}
 
